7cd6d38fcf96aab7c5b5e003148a783a374bdd10
alessandro <alessandrochetta@gmail.com>
2015-06-11 17:50:37 -0500

    5th pages commit

---
+++ Major-clinical-event-chart/data/data.js
@@ -0,0 +1,86 @@
+/**
+ * Created by alessandro on 6/4/15.
+ */
+var data1 = [
+    {
+        "bar_name":"pain",
+        "value":27
+    },
+    {
+        "bar_name":"bleeding",
+        "value":50
+    },
+    {
+        "bar_name":"fever",
+        "value":41
+    },
+    {
+        "bar_name":"breathing",
+        "value":100
+    },
+    {
+        "bar_name":"output",
+        "value":9
+    },
+    {
+        "bar_name":"consciuous",
+        "value":10
+    }
+];
+
+var data2 = [
+    {
+        "bar_name":"pain",
+        "value":100
+    },
+    {
+        "bar_name":"bleeding",
+        "value":30
+    },
+    {
+        "bar_name":"fever",
+        "value":20
+    },
+    {
+        "bar_name":"breathing",
+        "value":10
+    },
+    {
+        "bar_name":"output",
+        "value":66
+    },
+    {
+        "bar_name":"consciuous",
+        "value":9
+    }
+];
+
+
+var data3 = [
+    {
+        "bar_name":"pain",
+        "value":50
+    },
+    {
+        "bar_name":"bleeding",
+        "value":77
+    },
+    {
+        "bar_name":"fever",
+        "value":61
+    },
+    {
+        "bar_name":"breathing",
+        "value":10
+    },
+    {
+        "bar_name":"output",
+        "value":76
+    },
+    {
+        "bar_name":"consciuous",
+        "value":60
+    }
+];
+
+
