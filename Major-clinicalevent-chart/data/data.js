e79206d5f3ce9375b88da332a5c10ee38b68884c
alessandro <alessandrochetta@gmail.com>
2015-08-02 20:20:52 -0500

    delete

---
+++ Major-clinicalevent-chart/data/data.js
@@ -0,0 +1,138 @@
+/**
+ * Created by alessandro on 6/4/15.
+ */
+
+var data1 = [
+    {
+        "bar_name":"pain",
+        "value":27,
+        "clinical_events": [21,4,0,6,2,12,18,8,14]
+    },
+    {
+        "bar_name":"bleeding",
+        "value":16,
+        "clinical_events": [7,3,4,15,17,20]
+    },
+    {
+        "bar_name":"fever",
+        "value":41,
+        "clinical_events": [7,11,6,19,16]
+    },
+    {
+        "bar_name":"breathing",
+        "value":62,
+        "clinical_events": [20,13,18,4,6]
+    },
+    {
+        "bar_name":"output",
+        "value":9,
+        "clinical_events": [21,13,11,1,3]
+    },
+    {
+        "bar_name":"consciuous",
+        "value":10,
+        "clinical_events": [2,6,9,15,18,20]
+    }
+];
+
+/*
+var data1 = [
+    {
+        "bar_name":"pain",
+        "value":9,
+        "clinical_events": [0]
+    },
+    {
+        "bar_name":"bleeding",
+        "value":5,
+        "clinical_events": [0]
+    },
+    {
+        "bar_name":"fever",
+        "value":70,
+        "clinical_events": [0]
+    },
+    {
+        "bar_name":"breathing",
+        "value":23,
+        "clinical_events": [0]
+    },
+    {
+        "bar_name":"output",
+        "value":33,
+        "clinical_events": [0]
+    },
+    {
+        "bar_name":"conscious",
+        "value":6,
+        "clinical_events": [0]
+    }
+];*/
+
+var data2 = [
+    {
+        "bar_name":"pain",
+        "value":27,
+        "clinical_events": [0,2,4,21,14,12,18,2,6,8,10]
+    },
+    {
+        "bar_name":"bleeding",
+        "value":50,
+        "clinical_events": [7,3,4,15,17,20]
+    },
+    {
+        "bar_name":"fever",
+        "value":41,
+        "clinical_events": [7,11,6,19,16]
+    },
+    {
+        "bar_name":"breathing",
+        "value":100,
+        "clinical_events": [20,13,18,4,6]
+    },
+    {
+        "bar_name":"output",
+        "value":9,
+        "clinical_events": [0,9,11,1,3]
+    },
+    {
+        "bar_name":"consciuous",
+        "value":10,
+        "clinical_events": [2,6,9,15,18,20]
+    }
+];
+
+var data3 = [
+    {
+        "bar_name":"pain",
+        "value":27,
+        "clinical_events": [0,3,4,21,14,12,18,2,6,8,10]
+    },
+    {
+        "bar_name":"bleeding",
+        "value":50,
+        "clinical_events": [7,3,4,15,17,20]
+    },
+    {
+        "bar_name":"fever",
+        "value":41,
+        "clinical_events": [7,11,6,19,16]
+    },
+    {
+        "bar_name":"breathing",
+        "value":100,
+        "clinical_events": [20,13,18,4,6]
+    },
+    {
+        "bar_name":"output",
+        "value":9,
+        "clinical_events": [21,13,11,1,3]
+    },
+    {
+        "bar_name":"consciuous",
+        "value":10,
+        "clinical_events": [2,6,9,15,18,20]
+    }
+];
+
+
